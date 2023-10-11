c6f31d826ee4c804058e3bb98156cecad58eb9b5
Nikita Anna Ajith <116831077+nikita-anna@users.noreply.github.com>
2023-10-12 01:22:55 +0530

    Update Assignment-1.py

--- Assignment-1.py
+++ Assignment-1.py
@@ -1,3 +1,22 @@
+#Question 1
+dictionary={}
+n=int(input("Enter the number of words in the dictionary: "))
+for i in range(n):
+    word=input("Enter a word: ")
+    meaning=input("Enter the meaning: ")
+    dictionary[word]=meaning
+    ch=True
+while ch:
+    wrd=input("Enter the word whose meaning is to be searched: ")
+    for i in dictionary:
+        if i==wrd:
+            print("The meaning of",wrd,"is found as",dictionary[i])
+    ch=int(input("Do you want to search the meaning of another word? Enter 1 for yes and 0 for no: "))
+    if ch==1:
+         ch=True
+    else:
+         ch=False
+
 #Question 2
 def mul_table(num):
     for i in range(1,11):
